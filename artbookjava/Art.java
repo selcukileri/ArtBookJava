e472fe0d9adea4d2e4d6e755cf1c372299649e7c
selcukileri <135430990+selcukileri@users.noreply.github.com>
2023-06-03 15:52:35 +0300

    Add files via upload

---
+++ artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.selcukileri.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
